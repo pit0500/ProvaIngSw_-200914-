a877db46168ab1ae674344a97dfb5ec7006a2fc7
Pietro <pietro.macri>
2022-10-28 16:28:54 +0200

    Firs test cases commit

--- src/test/java/demo/FunnyAlgorithmTest.java
+++ src/test/java/demo/FunnyAlgorithmTest.java
@@ -2,6 +2,7 @@ package demo;
 
 import static org.junit.Assert.assertEquals;
 
+import org.joda.time.DateTime;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -21,28 +22,10 @@ public class FunnyAlgorithmTest {
 	private static int[] array = {5, 4, 8, 1, 2}; 
 	
 	@BeforeClass
-	public void testConstructor() {
-		System.out.println("testConstructor");
+	public static void testConstructor() {
+		System.out.println("Before class");
+		System.out.println(DateTime.now());
 		fa = new FunnyAlgorithms();
-	}
-	
-	@Test
-	public void stringToIntConverterWorks() {
-		assertEquals(821, fa.stringToIntConverter("821"));
-		assertEquals(233, fa.stringToIntConverter("233 "));
-	}
-	
-	@Test(expected = NumberFormatException.class)
-	public void stringToIntConverterThrowsNFException() {
-		System.out.println("stringToIntConverterThrowsNFException");
-		fa.stringToIntConverter("ew5425");
-		fa.stringToIntConverter("2.3");
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void stringToIntConverterThrowsIAException() {
-		System.out.println("stringToIntConverterThrowsIAException");
-		fa.stringToIntConverter("40000");
 	}
 	
 	
