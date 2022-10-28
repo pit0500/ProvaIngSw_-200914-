dd03d4461fdbf1ed5ea9e4f56fbfa00d53b876d8
Pietro <pietro.macri>
2022-10-28 16:29:30 +0200

    Firs test cases commit

--- src/test/java/demo/FunnyAlgorithmTest.java
+++ src/test/java/demo/FunnyAlgorithmTest.java
@@ -28,10 +28,7 @@ public class FunnyAlgorithmTest {
 		fa = new FunnyAlgorithms();
 	}
 	
-	@Test
-	public void stringToIntConverterWorksNormal() {
-		assertEquals(821, fa.stringToIntConverter("821"));
-	}
+}
 
 	
 	
